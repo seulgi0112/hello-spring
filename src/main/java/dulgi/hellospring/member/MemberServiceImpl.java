315e8ca98959043df1acbcd6692aa425709a4766
seulgi0112 <sara629718@gmail.com>
2021-04-09 16:45:35 +0900

    [ADD] 회원서비스 구현

---
+++ src/main/java/dulgi/hellospring/member/MemberServiceImpl.java
@@ -0,0 +1,47 @@
+package dulgi.hellospring.member;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberServiceImpl implements MemberService{
+	private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+
+	/**
+	 * 회원가입
+	 */
+	@Override
+	public Long join(Member member) {
+		validateDuplicateMember(member); //중복 회원 검증
+		 memberRepository.save(member);
+		 return member.getId();
+	}
+
+	/**
+	 * 중복가입 확인
+	 */
+	@Override
+	public void validateDuplicateMember(Member member) {
+		memberRepository.findByName(member.getName())
+		 .ifPresent(m -> {
+		 throw new IllegalStateException("이미 존재하는 회원입니다.");
+		 });		
+	}
+
+	/**
+	 * 전체회원조회
+	 */
+	@Override
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
+
+	/**
+	 * ID로 회원 조회
+	 */
+	@Override
+	public Optional<Member> findOne(Long memberId) {
+		return memberRepository.findById(memberId);
+	}
+	
+
+}
