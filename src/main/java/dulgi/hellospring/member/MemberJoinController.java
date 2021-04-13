1c7be220e318a90b95f0252d1739e563c6cb0fb1
seulgi0112 <sara629718@gmail.com>
2021-04-13 18:00:18 +0900

    [UPDATE] 오타 수정

--- src/main/java/dulgi/hellospring/member/MemberJoinController.java
+++ src/main/java/dulgi/hellospring/member/MemberJoinController.java
@@ -1,12 +1,10 @@
 package dulgi.hellospring.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
 public class MemberJoinController {
-	private MemberServiceImpl memberService;
+	private final MemberServiceImpl memberService;
 	
 	@Autowired
 	public MemberJoinController(MemberServiceImpl memberService) {
