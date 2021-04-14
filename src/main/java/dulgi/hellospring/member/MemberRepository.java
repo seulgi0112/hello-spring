119f727c4367bb84cac62d2b1e868d9e4d1ebe76
seulgi0112 <sara629718@gmail.com>
2021-04-14 15:47:32 +0900

    [UPDATE] 주석 추가

--- src/main/java/dulgi/hellospring/member/MemberRepository.java
+++ src/main/java/dulgi/hellospring/member/MemberRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 public interface MemberRepository {
 
 	Member save(Member member);
+	//Optional -> null을 반환할때
 	Optional<Member> findById(Long id);
 	Optional<Member> findByName(String name);
 	List<Member> findAll();
