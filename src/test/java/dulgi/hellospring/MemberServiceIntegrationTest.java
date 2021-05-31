14b492abe85371fcc52f745da38d35a4ae38cdac
seulgi0112 <sara629718@gmail.com>
2021-05-31 17:28:31 +0900

    [ADD] MemgerServiceIntegrationTest

---
+++ src/test/java/dulgi/hellospring/MemberServiceIntegrationTest.java
@@ -0,0 +1,50 @@
+package dulgi.hellospring;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import dulgi.hellospring.member.Member;
+import dulgi.hellospring.member.MemberRepository;
+import dulgi.hellospring.member.MemberService;
+
+@SpringBootTest
+@Transactional
+public class MemberServiceIntegrationTest {
+
+	@Autowired
+	MemberService memberService;
+	@Autowired
+	MemberRepository memberRepository;
+
+	@Test
+	public void 회원가입() throws Exception {
+		// Given
+		Member member = new Member();
+		member.setName("hello");
+		// When
+		Long saveId = memberService.join(member);
+		// Then
+		Member findMember = memberRepository.findById(saveId).get();
+		assertEquals(member.getName(), findMember.getName());
+	}
+
+	@Test
+	public void 중복_회원_예외() throws Exception {
+		// Given
+		Member member1 = new Member();
+		member1.setName("spring");
+		Member member2 = new Member();
+		member2.setName("spring");
+		// When
+		memberService.join(member1);
+		IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));// 예외 발생
+		//assertThat 적용 안됨
+		assertEquals(e.getMessage(), "이미 존재하는 회원입니다." );
+	}
+}
