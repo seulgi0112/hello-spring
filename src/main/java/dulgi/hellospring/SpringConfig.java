40274cfdc75cc525da000da3d8988a33e2370f2b
seulgi0112 <sara629718@gmail.com>
2021-05-31 17:13:23 +0900

    [UPDATE, ADD] memory 방식에서 H2 DB로 변경

--- src/main/java/dulgi/hellospring/SpringConfig.java
+++ src/main/java/dulgi/hellospring/SpringConfig.java
@@ -1,14 +1,22 @@
 package dulgi.hellospring;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import dulgi.hellospring.member.JdbcMemberRepository;
 import dulgi.hellospring.member.MemberRepository;
 import dulgi.hellospring.member.MemberService;
 import dulgi.hellospring.member.MemoryMemberRepository;
 
 @Configuration
 public class SpringConfig {
+	
+	private final DataSource dataSource;
+	 public SpringConfig(DataSource dataSource) {
+		 this.dataSource = dataSource;
+	 }
 	
 	@Bean
 	public MemberService memberService() {
@@ -17,7 +25,8 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
 	}
 	
 }
