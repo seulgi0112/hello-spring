a6f025779c89510e3aacf1efabbd824799c38e85
seulgi0112 <sara629718@gmail.com>
2021-05-31 15:38:20 +0900

    [ADD] Join Member Form

--- src/main/java/dulgi/hellospring/member/MemberController.java
+++ src/main/java/dulgi/hellospring/member/MemberController.java
@@ -2,17 +2,32 @@ package dulgi.hellospring.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 	private final MemberService memberService;
 
 	/*
-	 *  생성자에 @Autowired 가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다.
-	 *  이렇게객체 의존관계를 외부에서 넣어주는 것을 DI (Dependency Injection), 의존성 주입이라 한다.
+	 * 생성자에 @Autowired 가 있으면 스프링이 연관된 객체를 스프링 컨테이너에서 찾아서 넣어준다. 이렇게객체 의존관계를 외부에서 넣어주는
+	 * 것을 DI (Dependency Injection), 의존성 주입이라 한다.
 	 */
 	@Autowired
-	 public MemberController(MemberService memberService) {
-	    this.memberService = memberService;
-	 }
+	public MemberController(MemberService memberService) {
+		this.memberService = memberService;
+	}
+
+	@GetMapping(value = "/members/new")
+	public String createForm() {
+		return "members/createMemberForm";
+	}
+
+	@PostMapping(value = "/members/new")
+	public String create(MemberForm form) {
+		Member member = new Member();
+		member.setName(form.getName());
+		memberService.join(member);
+		return "redirect:/";
+	}
 }
