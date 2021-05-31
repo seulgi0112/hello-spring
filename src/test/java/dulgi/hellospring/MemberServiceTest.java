4e1ffb33e7b767709170ce0e2ed9481b5d98da0c
seulgi0112 <sara629718@gmail.com>
2021-05-31 15:14:07 +0900

    [UPDATE] 한글 수정

--- src/test/java/dulgi/hellospring/MemberServiceTest.java
+++ src/test/java/dulgi/hellospring/MemberServiceTest.java
@@ -25,7 +25,7 @@ class MemberServiceTest {
 		memberRepository.clearStore();
 	}
 	@Test
-	public void 회원가입() throws Exception {
+	public void joinMember() throws Exception {
 		//Given
 		Member member = new Member();
 		member.setName("hello");
@@ -36,7 +36,7 @@ class MemberServiceTest {
 		assertEquals(member.getName(), findMember.getName());
 	}
 	@Test
-	public void 중복_회원_예외() throws Exception {
+	public void duplicateMember() throws Exception {
 		//Given
 		Member member1 = new Member();
 		member1.setName("spring");
@@ -45,8 +45,8 @@ class MemberServiceTest {
 		//When
 		memberService.join(member1);
 		IllegalStateException e = assertThrows(IllegalStateException.class,
-				() -> memberService.join(member2));//예외가 발생해야 한다.
-		assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+				() -> memberService.join(member2));
+		assertThat(e.getMessage()).isEqualTo("Already used member name.");
 	}
 
 }
