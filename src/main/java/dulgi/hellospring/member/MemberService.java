21a4ee8e047fda2cef3a92f1185cf689877042ec
seulgi0112 <sara629718@gmail.com>
2021-06-02 17:49:41 +0900

    [UPDATE] clean code

--- src/main/java/dulgi/hellospring/member/MemberService.java
+++ src/main/java/dulgi/hellospring/member/MemberService.java
@@ -45,9 +45,8 @@ public class MemberService {
 	}
 
 	private void validateDuplicateMember(Member member) {
-		memberRepository.findByName(member.getName()).ifPresent(m -> {
-			throw new IllegalStateException("이미 존재하는 회원입니다.");
-		});
+		memberRepository.findByName(
+				member.getName()).ifPresent(m -> {throw new IllegalStateException("이미 존재하는 회원입니다.");});
 	}
 	
 	/*
